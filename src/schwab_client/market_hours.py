80029fc9ec4249c7811e59790f5eccdb35a1cd72
Dro <apinero1992@gmail.com>
2025-07-20 19:10:37 -0700

    fix: Updating end point classes template.

--- src/schwab_client/market_hours.py
+++ src/schwab_client/market_hours.py
@@ -1,7 +1,11 @@
 """Module for interacting with the Schwab API Market Hours endpoint."""
 
+from schwab_client.protocol import ClientProtocol
+
 
 class MarketHours:
     """Schwab API endpoint to obtain market hours."""
 
-    pass
+    def __init__(self, client: ClientProtocol) -> None:
+        """Market hours init method."""
+        pass
--- src/schwab_client/quotes/quotes.py
+++ src/schwab_client/quotes/quotes.py
@@ -1,7 +1,11 @@
 """Module for interacting with the Schwab Options endpoint."""
 
+from schwab_client.protocol import ClientProtocol
+
 
 class Quotes:
     """Schwab API equities stock quotes class."""
 
-    pass
+    def __init__(self, client: ClientProtocol) -> None:
+        """Quotes init method."""
+        pass
